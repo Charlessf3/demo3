dad9d17eb2bed21d5534fd4ecebeb7fb48a1d169
Charlessf3 <2623955134@qq.com>
2018-07-13 17:59:06 +0800

    180713

--- gittest.pro
+++ gittest.pro
@@ -32,3 +32,6 @@ HEADERS += \
 
 FORMS += \
         mainwindow.ui
+
+RESOURCES += \
+    myicon.qrc
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QPushButton>
+#include <QFileDialog>
+#include <QTextStream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    // 初始化文件为未保存状态
+    isUntitled = true;
+    // 初始化文件名为"未命名.txt"
+    curFile = tr("未命名.txt");
+    // 初始化窗口标题为文件名
+    setWindowTitle(curFile);
 }
 
 MainWindow::~MainWindow()
