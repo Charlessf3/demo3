f682df8901f99a5fc97f2f105c4dc79f7ea69690
Charlessf3 <2623955134@qq.com>
2018-07-16 13:46:38 +0800

    180716

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,7 +18,100 @@ MainWindow::MainWindow(QWidget *parent) :
     setWindowTitle(curFile);
 }
 
+
+
+void MainWindow::newFile()
+{
+    if(maybeSave())
+    {
+        isUntitled=true;
+        curFile=tr("未命名.txt");
+        setWindowTitle(curFile);
+        ui->textEdit->clear();
+        ui->textEdit->setVisible(true);
+    }
+}
+
+bool MainWindow::maybeSave()
+{
+    if(ui->textEdit->document()->isModified())
+    {
+        QMessageBox box;
+        box.setWindowTitle(tr("warning"));
+        box.setIcon(QMessageBox::Warning);
+        box.setText(curFile+tr("尚未保存，是否保存？"));
+        QPushButton *yesBtn=box.addButton(tr("是（&Y）"),QMessageBox::YesRole);
+        box.addButton(tr("否(&N)"),QMessageBox::NoRole);
+        QPushButton *cancelBut =box.addButton(tr("取消"),QMessageBox::RejectRole);
+        box.exec();
+        if(box.clickedButton()==yesBtn)
+            return save();
+        else
+            return false;
+    }
+    return true;
+}
+
+bool MainWindow::save()
+{
+    if(isUntitled)
+    {
+        return saveAs();
+    }
+    else
+    {
+        return saveFile(curFile);
+    }
+}
+
+bool MainWindow::saveAs()
+{
+    QString fileName=QFileDialog::getSaveFileName(this,tr("另存为"),curFile);
+    if(fileName.isEmpty())
+        return false;
+    return saveFile(fileName);
+}
+
+bool MainWindow::saveFile(const QString &fileName)
+{
+    QFile file(fileName);
+    if(!file.open(QFile::WriteOnly|QFile::Text))
+    {
+        QMessageBox::warning(this,tr("多文档编辑器"),tr("无法写入文件%1：/n %2").arg(fileName).arg(file.errorString()));
+        return false;
+    }
+    QTextStream out(&file);
+    QApplication::setOverrideCursor(Qt::WaitCursor);
+    out <<ui->textEdit->toPlainText();
+    QApplication::restoreOverrideCursor();
+    isUntitled=false;
+    curFile=QFileInfo(fileName).canonicalFilePath();
+    setWindowTitle(curFile);
+    return true;
+}
+
+
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_action_N_triggered()
+{
+    newFile();
+}
+
+void MainWindow::on_action_O_triggered()
+{
+
+}
+
+void MainWindow::on_action_S_triggered()
+{
+    save();
+}
+
+void MainWindow::on_action_A_triggered()
+{
+    saveAs();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -20,6 +20,15 @@ public:
     bool saveAs();    // 另存为操作
     bool saveFile(const QString &fileName); // 保存文件
 
+private slots:
+    void on_action_N_triggered();
+
+    void on_action_O_triggered();
+
+    void on_action_S_triggered();
+
+    void on_action_A_triggered();
+
 private:
     Ui::MainWindow *ui;
     // 为真表示文件没有保存过，为假表示文件已经被保存过了
